e4ec09161738f9980ff631bd36b99ae5216c4d24
Pavel_Stoyakin <patework@mail.ru>
2019-02-27 16:59:04 +0500

    -- begin index page

---
+++ app/js/common.js
@@ -0,0 +1,176 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", function (event) {
+  window.site = {};
+  window.site.form = {
+    init: function init() {
+      let _th = this,
+        form = document.querySelectorAll('form'),
+        fieldPhone = document.querySelectorAll('.js-phone');
+      for (let i = 0; i < fieldPhone.length; i++) {
+        fieldPhone[i].mask('+7(999) 999-9999');
+      }
+      for (let i = 0; i < form.length; i++) {
+        form[i].submit(function () {
+          if (!_th.checkForm(form[i])) return false;
+        });
+      }
+    },
+    checkForm: function checkForm(form) {
+      let checkResult = true;
+      form.find('.warning').removeClass('warning');
+      form.find('input, textarea, select').each(function () {
+        if ($(this).data('req')) {
+          switch ($(this).data('type')) {
+            case 'tel':
+              var re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+              if (!re.test($(this).val())) {
+                $(this).addClass('warning');
+                checkResult = false;
+              }
+              break;
+            case 'email':
+              var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+              if (!re.test($(this).val())) {
+                $(this).addClass('warning');
+                checkResult = false;
+              }
+              break;
+            case 'checkbox_personal':
+              if (!$(this).is(':checked')) {
+                $(this).parents('.checkbox').addClass('warning');
+                checkResult = false;
+              }
+              break;
+            default:
+              if ($.trim($(this).val()) === '') {
+                $(this).addClass('warning');
+                checkResult = false;
+              }
+              break;
+          }
+        }
+      });
+      return checkResult;
+    }
+  }.init();
+  window.site.obj = {
+    fadeOut: function fadeOut(selector, duration) {
+      let element = document.querySelector(selector),
+        op = 1;
+      if(!element)
+        return;
+      let timer = setInterval(function () {
+        if (op <= 0.1){
+            clearInterval(timer);
+            element.style.display = 'none';
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op -= op * 0.1;
+      }, duration/10 || 20);
+    },
+
+    fadeIn: function fadeIn(selector, duration) {
+      let element = document.querySelector(selector),
+        op = 0.1;
+      if(!element)
+        return;
+      element.style.opacity = 0;
+      element.style.display = 'block';
+      let timer = setInterval(function () {
+          if (op >= 1){
+              clearInterval(timer);
+          }
+          element.style.opacity = op;
+          element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+          op += op * 0.1;
+      }, duration/10 || 20);
+    },
+
+    map: function map() {
+      let $map = document.querySelector('.js-map'),
+        coords = $map.data('coords').split(',');
+      ymaps.ready(function () {
+        let myMap = new ymaps.Map("yaMap", {
+          center: [coords[0], coords[1]],
+          zoom: $map.data('zoom') || 14,
+          controls: ['largeMapDefaultSet']
+        });
+        myMap.controls.add('zoomControl', {
+          size: 'small'
+        });
+        myMap.behaviors.disable('scrollZoom');
+        let myPlacemark = new ymaps.Placemark(coords, {}, {
+          iconLayout: 'default#image',
+          iconImageHref: 'static/img/pin.png',
+          iconImageSize: [50, 66]
+        });
+        myMap.geoObjects.add(myPlacemark);
+      });
+      return;
+    },
+
+    tabs: function tabs() {
+      $('.imodels__tabs-nav-btn').on('click', function () {
+        var _t = $(this),
+          _tData = _t.data('tabs-nav'),
+          _tPar = _t.parents('.js-tabs'),
+          tabs = _tPar.find('.imodels__tabs-item'),
+          btn = _tPar.find('.imodels__tabs-nav-btn');
+        if (!_t.hasClass('imodels__tabs-nav-btn--active')) {
+          btn.removeClass('imodels__tabs-nav-btn--active');
+          _t.addClass('imodels__tabs-nav-btn--active');
+          tabs.fadeOut(350);
+          setTimeout(function () {
+            tabs.removeClass('imodels__tabs-nav-btn--active');
+            _tPar.find('.imodels__tabs-item[data-tabs-item="' + _tData + '"]').fadeIn(350);
+          }, 350);
+        }
+        return false;
+      });
+    },
+
+    burger: function burger() {
+      let _th = this,
+        $body = document.querySelector('body');
+      document.querySelector('.js-burger').addEventListener('click', function() {
+        let _t = this;
+        if (!_t.classList.contains('header__burger--active')) {
+          _t.classList.add('header__burger--active');
+          _th.fadeIn('.nav');
+          $body.classList.add('open-menu');
+        } else {
+          _t.classList.remove('header__burger--active');
+          _th.fadeOut('.nav');
+          $body.classList.remove('open-menu');
+        }
+        return false;
+      });
+    },
+
+    init: function init() {
+      let $body = document.querySelector('body'),
+        yaMap = document.querySelector('.js-map') || false,
+        tabs = document.querySelector('.js-tabs') || false,
+        burger = document.querySelector('.js-burger') || false;
+
+      //check ios
+      if (navigator.userAgent.match(/(iPod|iPhone|iPad)/)) {
+        $body.classList.add('ios');
+      }
+
+      if (yaMap)
+        //this.map();
+
+      if (tabs)
+        //this.tabs();
+
+      if (burger)
+        //this.burger();
+
+      return this;
+    }
+
+  }.init();
+});
---
+++ gulpfile.js
@@ -0,0 +1,276 @@
+var fs = require('fs'),
+  gulp = require('gulp'),
+  rebaseCSSurls = require('gulp-rebase-css-urls'),
+  pug = require('gulp-pug'),
+  pugInheritance = require('gulp-pug-inheritance'),
+  autoprefixer = require('autoprefixer'),
+  browserSync = require('browser-sync').create(),
+  reload = browserSync.reload,
+  print = require('gulp-print').default,
+  notify = require('gulp-notify'),
+  stylus = require('gulp-stylus'),
+  plumber = require('gulp-plumber'),
+  $ = require('gulp-load-plugins')();
+
+// Пути
+var src = {
+  'app': {
+    'stylus': 'app/stylus/',
+    'css': 'app/css/',
+    'js': 'app/js/',
+    'vendor': 'app/vendor/',
+    'pug': 'app/pug/'
+  },
+  'build': {
+    'css': 'prod/static/css/',
+    'js': 'prod/static/js/',
+    'tmpl': 'prod'
+  }
+};
+
+// Билд CSS
+function css(src, dst) {
+  return function () {
+    return gulp.src(src)
+      .pipe($.plumber({
+        errorHandler: $.notify.onError({
+          title: 'CSS error',
+          message: '<%= error.message %>'
+        })
+      }))
+      .pipe(print())
+      .pipe(rebaseCSSurls('.'))
+      .pipe($.postcss([
+        autoprefixer({
+          browsers: [
+            'last 2 versions',
+            'Android >= 4',
+            'IE >= 9'
+          ]
+        })
+      ]))
+      .pipe($.concat('styles.css'))
+      .pipe(gulp.dest(dst))
+      .pipe($.notify({
+        title: 'CSS',
+        message: 'CSS build complete',
+        onLast: true
+      }))
+      .pipe(reload({stream:true}));
+  }
+}
+
+// Упаковка CSS
+function csspack(src, dst) {
+  return function () {
+    return gulp.src(src)
+      .pipe($.plumber({
+        errorHandler: $.notify.onError({
+          title: 'CSS PACK error',
+          message: '<%= error.message %>'
+        })
+      }))
+      .pipe(print())
+      .pipe($.postcss([
+        require('cssnano')({
+          discardComments: {
+            removeAll: true
+          },
+          discardUnused: {
+            namespace: false
+          }
+        })
+      ]))
+      .pipe($.concat('styles.pack.css'))
+      .pipe(gulp.dest(dst))
+      .pipe($.notify({
+        title: 'CSS PACK',
+        message: 'CSS PACK complete',
+        onLast: true
+      }))
+      .pipe(reload({stream: true}));
+  }
+}
+
+// Билд JS
+function js(src, dst, fileName) {
+  return function () {
+    return gulp.src(src)
+      .pipe($.sourcemaps.init())
+      .pipe($.babel({
+        compact: false,
+        presets: ["@babel/preset-env"],
+        ignore: ['what-input.js', 'swiper.js']
+      }))
+      .pipe($.plumber({
+        errorHandler: $.notify.onError({
+          title: 'JS error',
+          message: '<%= error.message %>'
+        })
+      }))
+      .pipe($.notify({
+        title: 'JS-babel',
+        message: 'JS-babel complete'
+      }))
+      .pipe($.changed(dst, {
+        extension: '.js'
+      }))
+      .pipe(print())
+      .pipe($.concat(fileName))
+      //.pipe($.uglify())
+      .pipe($.sourcemaps.write('./'))
+      .pipe(gulp.dest(dst))
+      .pipe($.notify({
+        title: 'JS',
+        message: 'JS build complete',
+        onLast: true
+      }))
+      .pipe(reload({stream: true}));
+  }
+}
+
+function jsVendor(src, dst, fileName) {
+  return function () {
+    return gulp.src(src)
+      .pipe($.sourcemaps.init())
+      .pipe($.plumber({
+        errorHandler: $.notify.onError({
+          title: 'JS error',
+          message: '<%= error.message %>'
+        })
+      }))
+      .pipe($.changed(dst, {
+        extension: '.js'
+      }))
+      .pipe(print())
+      .pipe($.concat(fileName))
+      //.pipe($.uglify())
+      .pipe($.sourcemaps.write('./'))
+      .pipe(gulp.dest(dst))
+      .pipe($.notify({
+        title: 'JS-vendor',
+        message: 'JS-vendor complete',
+        onLast: true
+      }))
+      .pipe(reload({stream: true}));
+  }
+}
+
+// Таски
+// Компилим pug
+gulp.task('pug', function buildHTML() {
+  return gulp.src(src.app.pug + '*.pug')
+    .pipe($.changed(src.build.tmpl, { extension: '.html' }))
+    .pipe($.if(global.isWatching, $.cached('pug')))
+    .pipe(pugInheritance({
+      basedir: 'app/pug/',
+      skip: 'node_modules'
+    }))
+    .pipe(pug({pretty: '    '}))
+    .pipe(gulp.dest(src.build.tmpl))
+    .pipe(reload({stream: true}));
+});
+
+// Компилим CSS
+gulp.task('stylus', function () {
+  return gulp.src(src.app.stylus + 'styles.styl')
+    .pipe(plumber({
+      errorHandler: function (err) {
+        notify.onError({
+          title: 'Styles compilation error',
+          message: err.messagegulp
+        })(err);
+        this.emit('end');
+      }
+    }))
+    .pipe(print())
+    .pipe(stylus({
+      'include css': true,
+      compress: false
+    }))
+    .pipe(gulp.dest(src.app.css))
+    .pipe($.notify({
+      title: 'STYLUS',
+      message: 'STYLUS build complete',
+      onLast: true
+    }));
+});
+
+// Собираем CSS
+gulp.task('css', css([
+    // тут нужно добавить css-файлы сторонних либ, если они используются
+    src.app.css + 'fonts.css',
+    src.app.css + 'ext/swiper-cut-svg.css',
+    // 'node_modules/jquery-form-styler/dist/jquery.formstyler.css',
+    // 'node_modules/jquery-form-styler/dist/jquery.formstyler.theme.css',
+    src.app.css + 'styles.css'
+  ],
+  src.build.css
+));
+
+// Пакуем CSS
+gulp.task('csspack', csspack([src.build.css + 'styles.css'], src.build.css));
+
+// Собираем JS
+gulp.task('js-own', js([ src.app.js + 'common.js' ], src.build.js, 'own.js'));
+
+gulp.task('js-vendor', js(
+  [
+    'node_modules/jquery/dist/jquery.js',
+    'node_modules/jquery-ui-dist/jquery-ui.min.js',
+    'node_modules/jquery-migrate/dist/jquery-migrate.min.js',
+    //'node_modules/systemjs/dist/system.js',
+    //'node_modules/babel-polyfill/dist/polyfill.js',
+    'node_modules/swiper/dist/js/swiper.js',
+    // 'node_modules/jquery-form-styler/dist/jquery.formstyler.min.js',
+    // src.app.js + 'ext/jquery.maskedinput.min.js',
+    //подключаем модерниз, если нужен
+    //src.app.vendor + "modernizr/modernizr.custom.js"
+  ],
+  src.build.js,
+  'vendor.js'
+  )
+);
+
+gulp.task(
+  'js',
+  function () {
+    gulp.start('js-vendor');
+    gulp.start('js-own');
+  }
+);
+
+gulp.task('setWatch', function () {
+  global.isWatching = true;
+});
+
+// browser-sync task for starting the server.
+gulp.task('browserSync', function () {
+  browserSync.init({
+    server: {
+      baseDir: "./prod",
+      directory: true
+    }
+  });
+});
+
+// Reload all Browsers
+gulp.task('bsReload', function () {
+  browserSync.reload();
+});
+
+// Watch-таск для работы
+gulp.task('watch', function () {
+  gulp.start('browserSync');
+  gulp.watch(src.app.pug + '*.pug', ['setWatch', 'pug']);
+  gulp.watch(src.app.stylus + '**/*.styl', ['stylus']);
+  gulp.watch(src.app.css + 'styles.css', ['css']);
+  //  gulp.watch(src.build.css + 'styles.css', ['csspack']);
+  gulp.watch(src.app.js + '**/*.js', ['js-own']);
+});
+
+// Сборка проекта
+gulp.task('build', ['setWatch', 'pug', 'stylus', 'css', 'js']);
+
+// Дефолтный таск
+gulp.task('default', ['build', 'browserSync']);
